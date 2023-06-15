5cd2affdd2032288377411ea6244416d0b60b97a
yeeun <yeeeun910@gmail.com>
2023-06-16 03:24:36 +0900

    (+ pet_component 파일 추가 커밋)

--- lib/screens/pet/view/pet_component.dart
+++ lib/screens/pet/view/pet_component.dart
@@ -1,104 +1,98 @@
+import 'package:dahae_mobile/common/styles/app_colors.dart';
+import 'package:dahae_mobile/common/styles/app_text_style.dart';
 import 'package:flutter/material.dart';
 
-class MyClipper extends CustomClipper<Rect> {
+class PetFooter extends StatefulWidget {
   @override
-  Rect getClip(Size size) {
-    return Rect.fromLTWH(0, 0, size.width, size.height);
-  }
+  _PetFooterState createState() => _PetFooterState();
 
-  // Rect getCircle(Size size) {
-  //   return Rect.fromCircle(center: center, radius: radius);
+  // void updateExp() {
+  //   setState(() {});
   // }
-
-  @override
-  bool shouldReclip(CustomClipper<Rect> oldClipper) {
-    return false;
-  }
 }
 
-class BannerCircle extends StatelessWidget {
-  BannerCircle({
-    super.key,
-    required this.label,
-    required this.main,
-    this.labelEnd = '',
-  });
-
-  final String label;
-  final String main;
-  final String labelEnd;
-
+class _PetFooterState extends State<PetFooter> {
   @override
   Widget build(BuildContext context) {
-    ThemeData themeData = Theme.of(context);
-
-    return ClipOval(
-      clipper: MyClipper(),
-      child: Container(
-        height: 80,
-        width: 80,
-        color: Color(0xFFFEFEFE),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              label,
-              style: themeData.textTheme.subtitle2?.copyWith(fontSize: 9),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 2),
-              child: Text(
-                main,
-                style: labelEnd != ''
-                    ? themeData.textTheme.bodyText1
-                        ?.copyWith(fontWeight: FontWeight.w700, fontSize: 16)
-                    : themeData.textTheme.bodyText1
-                        ?.copyWith(fontWeight: FontWeight.w700, fontSize: 20),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
-              child: labelEnd != ''
-                  ? Text(
-                      labelEnd,
-                      style:
-                          themeData.textTheme.subtitle2?.copyWith(fontSize: 9),
-                    )
-                  : Container(),
-            )
-          ],
-        ),
+    return Container(
+      height: 252,
+      //color: AppColors.highlightColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          PetStats(),
+          const SizedBox(height: 6),
+        ],
       ),
     );
   }
 }
 
-class PetBanner extends StatelessWidget {
-  const PetBanner({super.key});
+class PetStats extends StatefulWidget {
+  @override
+  _PetStatsState createState() => _PetStatsState();
+}
+
+class _PetStatsState extends State<PetStats> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(vertical: 9, horizontal: 30),
+      decoration: BoxDecoration(
+        color: AppColors.highlightColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        PercentWrapper(
+          flex: 1,
+          title: "애정도",
+          value: 50,
+        ),
+        const SizedBox(width: 16),
+        PercentWrapper(
+          flex: 2,
+          title: "경험치",
+          value: 100,
+        ),
+      ]),
+    );
+  }
+}
+
+class PercentWrapper extends StatelessWidget {
+  final int flex;
+  final String title;
+  final int value;
+
+  PercentWrapper(
+      {super.key,
+      required this.flex,
+      required this.title,
+      required this.value});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return Expanded(
+      flex: flex,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 6),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            BannerCircle(label: '애정도', main: '80%'),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 7),
-              child: BannerCircle(
-                label: '현재는',
-                main: '유아기',
-                labelEnd: '입니다.',
-              ),
-            ),
-            BannerCircle(label: '경험치', main: '80%'),
+            Text(title,
+                textAlign: TextAlign.start, style: AppTextStyle.subTitle10),
+            Text("$value %",
+                textAlign: TextAlign.center, style: AppTextStyle.pointNumber),
           ],
         ),
-        SizedBox(height: 45),
-      ],
+      ),
     );
   }
 }
